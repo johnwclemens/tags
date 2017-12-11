d49f4970c2b86eada90d743bd183335280618956
John Clemens <John.Clemens@Thomsonreuters.com>
2017-12-11 12:41:21 -0600

    try reDate

--- tags.py
+++ tags.py
@@ -1,6 +1,9 @@
 #Funktional Family At Kiss The Sky Batavia IL 12-03-17 (Camera 1)
 import os, sys, re
 
+cres = {}
+reDate       = r'\s*(?P<reDate>\d{2}-\d{2}-\d{2})\s*'
+
 def main():
     with open('tags.txt', 'w') as outFile:
         with open('titles.txt', 'r') as inFile:
@@ -28,7 +31,21 @@ def genTags(title, outFile):
         name = title[:idx-1]
         print('{}'.format(name), file=outFile)
         tags.append(name)
+    key = 'date'
+    pattern = reDate
+    m = findString(title, cres, key, pattern)
+    if m:
+        grpMap = m.groupdict()
+        if 'reDate' in grpMap:
+            date = m.group('reDate')
+            print('date={}'.format(date))
+            tags.append(date)
     return tags
+
+def findString(s, cres, key, pattern):
+    if key not in cres:
+        cres[key] = re.compile(pattern)
+    return cres[key].search(s)
 
 def printn(msg='', sep=' ', end='\n', file=sys.stdout, flush=False):
     print(msg, sep=sep, end=end, file=file, flush=flush)
