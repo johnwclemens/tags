78d5654a4338a1200df9ed7b8cf0ac715868025e
John Clemens <Jwclemens@yahoo.com>
2017-12-12 00:30:55 -0600

    try more re, a

--- tags.py
+++ tags.py
@@ -2,7 +2,9 @@
 import os, sys, re
 
 cres = {}
+reAt         = r'\s*(?P<reAt>at|At)\s*'
 reDate       = r'\s*(?P<reDate>\d{2}-\d{2}-\d{2})\s*'
+reName       = r'\s*(\S+)\s*'
 
 def main():
     with open('tags.txt', 'w') as outFile:
@@ -26,20 +28,31 @@ def genTags(title, outFile):
     for word in words:
         printn('{}'.format(word), end=' ', file=outFile)
     printn(',', file=outFile)
-    idx = title.find('At')
-    if idx != -1:
-        name = title[:idx-1]
-        print('{}'.format(name), file=outFile)
-        tags.append(name)
+#    idx = title.find('At')
+#    if idx != -1:
+#        name = title[:idx-1]
+#        print('{}'.format(name), file=outFile)
+#        tags.append(name)
+    printn("title={}".format(title))
     key = 'date'
-    pattern = reDate
+    pattern = reName + reName + reAt + reName# + reDate
     m = findString(title, cres, key, pattern)
     if m:
+        print('m={}'.format(m))
+        print('group(1)={}'.format(m.group(1)))
         grpMap = m.groupdict()
+        if 'reAt' in grpMap:
+            at = m.group('reAt')
+            print('at={}'.format(at))
+            tags.append(at)
         if 'reDate' in grpMap:
             date = m.group('reDate')
             print('date={}'.format(date))
             tags.append(date)
+        if 'reName' in grpMap:
+            name = m.group('reName')
+            print('name={}'.format(name))
+            tags.append(name)
     return tags
 
 def findString(s, cres, key, pattern):
