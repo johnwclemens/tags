f109799cc42c54285a52e8b60e723ee05f06a406
John Clemens <Jwclemens@yahoo.com>
2017-12-13 12:48:15 -0600

    more re, b

--- tags.py
+++ tags.py
@@ -2,9 +2,18 @@
 import os, sys, re
 
 cres = {}
-reAt         = r'\s*(?P<reAt>at|At)\s*'
+reAt         = r'(?P<reAt>at|At)\s+'
+#reAt         = r'\s*(?P<reAt>at|At)\s*'
 reDate       = r'\s*(?P<reDate>\d{2}-\d{2}-\d{2})\s*'
-reName       = r'\s*(\S+)\s*'
+#reName       = r'\s*(\S+)\s*'
+#reName       = r'(\s*\S+){1,4}'
+#reName       = r'(?P<reName>\s*\S+){1,4}'
+#reName1       = r'(?P<reName1>\s*\S+)'
+#reName2       = r'(?P<reName2>\s*\S+)'
+#reName1       = r'(?P<reName1>\S+\s+)'
+#reName2       = r'(?P<reName2>\S+\s+)'
+#reName        = r'(?P<reName>\S+\s+){1,4}'
+reName        = r'(?P<reName>\S+\s+)+'
 
 def main():
     with open('tags.txt', 'w') as outFile:
@@ -35,12 +44,17 @@ def genTags(title, outFile):
 #        tags.append(name)
     printn("title={}".format(title))
     key = 'date'
-    pattern = reName + reName + reAt + reName# + reDate
+    pattern = reName + reAt# + reName + reAt + reName# + reDate
+    print('pattern={}'.format(pattern))
     m = findString(title, cres, key, pattern)
     if m:
         print('m={}'.format(m))
-        print('group(1)={}'.format(m.group(1)))
+        print('group(0)={}, group(1)={}, group(2)={}'.format(m.group(0), m.group(1), m.group(2)))
         grpMap = m.groupdict()
+        if 'reName' in grpMap:
+            name = m.group('reName')
+            print('name={}'.format(name))
+            tags.append(name)
         if 'reAt' in grpMap:
             at = m.group('reAt')
             print('at={}'.format(at))
@@ -49,10 +63,14 @@ def genTags(title, outFile):
             date = m.group('reDate')
             print('date={}'.format(date))
             tags.append(date)
-        if 'reName' in grpMap:
-            name = m.group('reName')
-            print('name={}'.format(name))
-            tags.append(name)
+        if 'reName1' in grpMap:
+            name1 = m.group('reName1')
+            print('name1={}'.format(name1))
+            tags.append(name1)
+        if 'reName2' in grpMap:
+            name2 = m.group('reName2')
+            print('name2={}'.format(name2))
+            tags.append(name2)
     return tags
 
 def findString(s, cres, key, pattern):
