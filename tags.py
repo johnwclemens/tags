0eaecfcfa718d167d1453a1c4fe94598c3b83b46
John Clemens <Jwclemens@yahoo.com>
2017-12-28 23:33:03 -0600

    refactor o -> class

--- tags.py
+++ tags.py
@@ -3,13 +3,17 @@ import os, sys, re
 reDate       = r'\s*(?P<reDate>\d{1,2}-\d{1,2}-\d{2})\s*'
 
 def main():
-    Tags()
+    tags = Tags(inFileName='titles.txt', outFileName='tags.txt')
+    tags.run()
 
 class Tags(object):
     def __init__(self, inFileName = 'titles.txt', outFileName='tags.txt'):
+        self.tags = None
         self.cres = {}
         self.inFileName = inFileName
         self.outFileName = outFileName
+
+    def run(self):
         with open(self.outFileName, 'w') as self.outFile, open(self.inFileName, 'r') as self.inFile:
             self.getTitles()
         print('inFile={}'.format(self.inFile))
@@ -18,19 +22,19 @@ class Tags(object):
         print('outFile={}'.format(self.outFile))
         self.outFile = None
         print('outFile={}'.format(self.outFile))
-    
+
     def getTitles(self):
         for line in self.inFile:
-            tags = self.getTags(line.strip())
+            self.getTags(line.strip())
             printn('tags = {', file=self.outFile)
-            for k in tags.keys(): printn('    {} => {},'.format(k, tags[k]), file=self.outFile)
+            for k in self.tags.keys(): printn('    {} => {},'.format(k, self.tags[k]), file=self.outFile)
             printn("}", file=self.outFile)
 
     def getTags(self, line):
         printn('line = {}'.format(line), file=self.outFile)
-        tags = {}
+        self.tags = {}
         title = self.titleCase(line)
-        tags['Title'] = title
+        self.tags['Title'] = title
     #    words = title.split()
     #    printn('words = [', file=self.outFile, end='')
     #    for w in words:
@@ -39,23 +43,22 @@ class Tags(object):
     #        printn(' {}'.format(w), file=self.outFile, end='')
     #    printn(']', file=self.outFile)
     ##    tags['Title'] = ' '.join(words)
-        remainder = self.parse(title, ' At ', ['Name'], tags)
-        remainder = self.parse(remainder, ', ', ['Venue', 'City'], tags)
-        self.getDate(remainder, tags)
-        return tags
+        remainder = self.parse(title, ' At ', ['Name'])
+        remainder = self.parse(remainder, ', ', ['Venue', 'City'])
+        self.getDate(remainder)
 
     def titleCase(self, s):
         printn('titleCase({})'.format(s))
     #    return re.sub(r"([A-Za-z]+)", lambda mo: mo.group(0)[0].upper() + mo.group(0)[1:].lower(), s)
         return re.sub(r"[A-Za-z]+('[A-Za-z]+)?", lambda mo: mo.group(0)[0].upper() + mo.group(0)[1:].lower(), s)
 
-    def parse(self, s, delim, keys, tags):
+    def parse(self, s, delim, keys):
         max = len(keys)
         tokens = s.split(delim, max+1)
     #    printn('parse(delim = [{}], keys = {})'.format(delim, keys), file=self.outFile)
         printn('parse([{}], delim = [{}], keys = ['.format(s, delim), file=self.outFile, end='')
         for i in range(0, max):
-            tags[keys[i]] = tokens[i]
+            self.tags[keys[i]] = tokens[i]
             printn(' {}'.format(keys[i]), file=self.outFile, end='')
         printn('])', file=self.outFile)
         for i in range(0, max):
@@ -65,7 +68,7 @@ class Tags(object):
         printn('    [{}], {} = {}'.format(i, 'Remainder', tokens[i]), file=self.outFile)
         return remainder
 
-    def getDate(self, dStr, tags):
+    def getDate(self, dStr):
         m = self.findString(dStr, 'reDate', reDate)
         if m:
             print('m={}'.format(m))
@@ -73,7 +76,7 @@ class Tags(object):
             if 'reDate' in grpMap:
                 date = m.group('reDate')
                 print('    date = {}'.format(date), file=self.outFile)
-                tags['Date'] = date
+                self.tags['Date'] = date
 
     def findString(self, s, key, pattern):
         if key not in self.cres:
