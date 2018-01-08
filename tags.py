8b9008598d9742ea3077fd9843cf0c25a4c20018
John Clemens <Jwclemens@yahoo.com>
2018-01-08 00:02:00 -0600

    refactor w add prepare()

--- tags.py
+++ tags.py
@@ -15,7 +15,7 @@ class Tags(object):
 
     def run(self):
         with open(self.outFileName, 'w') as self.outFile, open(self.inFileName, 'r') as self.inFile:
-            self.getTitles()
+            self.readFile()
         self.close(self.inFileName, self.inFile)
         self.close(self.outFileName, self.outFile)
 
@@ -24,7 +24,7 @@ class Tags(object):
         file = None
         print('{}={}'.format(fileName, file))
 
-    def getTitles(self):
+    def readFile(self):
         i = 0
         for line in self.inFile:
             self.getTags(line.strip())
@@ -44,15 +44,15 @@ class Tags(object):
         printn('line = {}'.format(line), file=self.outFile)
         self.tags = collections.OrderedDict()
         title = self.titleCase(line)
+        title = self.prepare(title)
         self.addTag('Title', title)
         remainder = self.parse(title, ' At ', ['Name'])
         remainder = self.parse(remainder, ', ', ['Venue', 'City'])
-        remainder = self.getStateAndDate(remainder)
+        remainder = self.getStateDateOther(remainder)
         self.group()
 
     def addTag(self, key, value):
         self.tags[key] = value
-#        self.len += len(value)
 
     def parse(self, s, delim, keys):
         max = len(keys)
@@ -69,20 +69,20 @@ class Tags(object):
         printn('    [{}], {} = {}'.format(i, 'Remainder', tokens[i]), file=self.outFile)
         return remainder
 
-    def getStateAndDate(self, s):
-        m = self.findString(s, 'reDate', self.reDate)
-        self.addTag('Date', m.group(1) + '-' + m.group(2) + '-' + m.group(3))
+    def getStateDateOther(self, s):
+        m = self.reSearch(s, 'reDate', self.reDate)
         self.addTag('State', s[:m.start()])
+        self.addTag('Date', m.group(1) + '-' + m.group(2) + '-' + m.group(3))
         remainder = s[m.end():]
         if remainder: self.tags['Other'] = remainder
         return remainder
 
     def group(self):
-        self.addTag('Name_City', self.tags['Name'] + " " + self.tags['City'])
         self.addTag('Name_Date', self.tags['Name'] + " " + self.tags['Date'])
         self.addTag('Name_Venue', self.tags['Name'] + " " + self.tags['Venue'])
-        self.addTag('Name_Venue_City', self.tags['Name'] + " " + self.tags['Venue'] + " " + self.tags['City'])
+        self.addTag('Name_City', self.tags['Name'] + " " + self.tags['City'])
         self.addTag('Name_Venue_Date', self.tags['Name'] + " " + self.tags['Venue'] + " " + self.tags['Date'])
+        self.addTag('Name_Venue_City', self.tags['Name'] + " " + self.tags['Venue'] + " " + self.tags['City'])
         self.addTag('Name_City_Date', self.tags['Name'] + " " + self.tags['City'] + " " + self.tags['Date'])
         self.addTag('City_Date', self.tags['City'] + " " + self.tags['Date'])
         self.addTag('Venue_Date', self.tags['Venue'] + " " + self.tags['Date'])
@@ -92,13 +92,21 @@ class Tags(object):
         self.addTag('Name_Venue_Live', self.tags['Name'] + " Live " + self.tags['Venue'])
         self.addTag('Name_Venue_Date_Live', self.tags['Name'] + " Live " + self.tags['Venue'] + " " + self.tags['Date'])
 
-    def findString(self, s, key, pattern):
+    def reSearch(self, s, key, pattern):
         if key not in self.cres:
             self.cres[key] = re.compile(pattern)
         return self.cres[key].search(s)
 
     def titleCase(self, s):
         return re.sub(r"[A-Za-z]+('[A-Za-z]+)?", lambda mo: mo.group(0)[0].upper() + mo.group(0)[1:].lower(), s)
+
+    def prepare(self, s):
+        return re.sub(r'\((.*?)\)', r'\1', s)
+#        rePrepare = r'\((.*)\)'
+#        m = self.reSearch(s, 'prepare', rePrepare)
+#        if m: printn('prepare m = {}, m[0] = {},  m[1] = {}'.format(m.groups(), m.group(0), m.group(1)), file=self.outFile)
+#        else: printn('prepare no match for s = {}'.format(s), file=self.outFile)
+#        return re.sub(rePrepare, r'\1', s)
 
 def printn(msg='', sep=' ', end='\n', file=sys.stdout, flush=False):
     print(msg, sep=sep, end=end, file=file, flush=flush)
