37bb55a97362ef8d6182d1d4592cf1dc3832100c
John Clemens <Jwclemens@yahoo.com>
2018-05-30 14:51:21 -0500

    products 2

--- products.py
+++ products.py
@@ -8,25 +8,28 @@ def getProds(a=None):
     for i in range(1, la):
         print(',{}'.format(a[i]), end='')
     print(')')
-    lp, rp = 1, 1
+    lp, rp, r = 1, 1, []
     for i in range(0, la):
-        lp *= a[i]
-#        rp = 
-    r = lp * rp
+        if i > 0:
+            lp *= a[i-1]
+        if i < la-1:
+            rp *= a[i+1]
+        r.append(lp*rp)
     return r
 #    return aDefault[0]
     
 if __name__ == "__main__":
-    inP = (5, 4, 8, 6, 3)
+#    inP = (5, 4, 8, 6, 3)
 #    inP = [2, 1]
 #    inP = [2,]
-#    inP = []
+    inP = []
     outP = getProds(a=inP)
 #    outP = getProds()
 #    outP = getProds(a=None)
     print('prod = {}'.format(outP))
     
-4, 7, 3, 2, 5
+'''
+4, 7, 3, 2, 5 PROD=840
 -------------------------------------------------------------------------
 | I |     LEFT    |   RIGHT    | LEFT INDEX | RIGHT INDEX | 0->I | I->L |
 |---|-------------|------------|-----------------------------------------
@@ -36,3 +39,6 @@ if __name__ == "__main__":
 | 3 |  4, 7, 3    |          5 | 0, 1, 2    |          4  | 0->2 | 4->4 |
 | 4 |  4, 7, 3, 2 | 1          | 0, 1, 2, 3 | N           | 0->3 |      |
 -----------------------------------------------------------------|------|
+840/4, 840/7, 840/3, 840/2, 840/5
+210, 120, 280, 420, 168
+'''
